7a134c22c36fc5e9e0af5f9bd78d78f2876fab83
Casper Van Gheluwe <caspervg@gmail.com>
2026-03-03 17:22:39 +0100

    refactor: remove dead filter panel base

--- src/dll/PropPanelTab.hpp
+++ src/dll/PropPanelTab.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <unordered_map>
 #include <vector>
-#include "FilterableTablePanel.hpp"
 #include "PanelTab.hpp"
 #include "PropFilterHelper.hpp"
 #include "PropPainterInputControl.hpp"
 #include "ThumbnailCache.hpp"
 #include "public/ImGuiTexture.h"
 
-class PropPanelTab : public FilterableTablePanel, public PanelTab {
+class PropPanelTab : public PanelTab {
 public:
     PropPanelTab(SC4PlopAndPaintDirector* director,
                  LotRepository* lots, PropRepository* props, FavoritesRepository* favorites,
@@ -25,15 +24,14 @@ public:
 
     [[nodiscard]] const char* GetTabName() const override;
     void OnRender() override;
-    void OnInit() override {}
     void OnShutdown() override { thumbnailCache_.Clear(); }
     void OnDeviceReset(uint32_t deviceGeneration) override;
 
 private:
     ImGuiTexture LoadPropTexture_(uint64_t propKey);
 
-    void RenderFilterUI_() override;
-    void RenderTable_() override;
+    void RenderFilterUI_();
+    void RenderTable_();
 
     void RenderTableInternal_(const std::vector<PropView>& filteredProps,
                               const std::unordered_set<uint64_t>& favorites);
